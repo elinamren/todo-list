dc9c5297a2cb22b9d00328a50c571bfeece3c904
Elin Amrén <65346494+elinamren@users.noreply.github.com>
2020-10-07 12:08:45 +0200

    todo list

---
+++ app.js
@@ -0,0 +1,46 @@
+//Selectors
+const todoInput = document.querySelector(".todo-input");
+const todoBtn = document.querySelector(".todo-btn");
+const todoList = document.querySelector(".todo-list");
+
+//Eventlisteners
+todoBtn.addEventListener("click", addTodo);
+todoList.addEventListener("click", deleteChecked);
+
+//Functions
+function addTodo(event) {
+  event.preventDefault();
+
+  const todoDiv = document.createElement("div");
+  todoDiv.classList.add("todo");
+
+  const newTodo = document.createElement("li");
+  newTodo.innerText = todoInput.value;
+  newTodo.classList.add("todo-item");
+  todoDiv.appendChild(newTodo);
+
+  const checkedBtn = document.createElement("button");
+  checkedBtn.innerHTML = '<i class="fas fa-check"></i>';
+  checkedBtn.classList.add("check-btn");
+  todoDiv.appendChild(checkedBtn);
+
+  const trashBtn = document.createElement("button");
+  trashBtn.innerHTML = '<i class="fas fa-trash"></i>';
+  trashBtn.classList.add("trash-btn");
+  todoDiv.appendChild(trashBtn);
+
+  todoList.appendChild(todoDiv);
+  todoInput.value = "";
+}
+
+function deleteChecked(e) {
+  const item = e.target;
+  if (item.classList[0] === "trash-btn") {
+    const todo = item.parentElement;
+    todo.remove();
+  }
+  if (item.classList[0] === "check-btn") {
+    const todo = item.parentElement;
+    todo.classList.toggle("checked");
+  }
+}
